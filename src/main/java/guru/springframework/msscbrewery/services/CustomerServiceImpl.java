a6902e4c40957402e024d608ca73f75add833950
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2022-04-09 11:14:14 +0530

    added the http operations for customer as well

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j //logger
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -12,5 +14,23 @@ public class CustomerServiceImpl implements CustomerService {
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("Nikhil Singh")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerID, CustomerDto customerDto) {
+        //todo impl
+        log.debug("Updating customer...");
+
+    }
+
+    @Override
+    public void deleteById(UUID customerID) {
+        log.debug("Deleting customer...");
     }
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -3,13 +3,11 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -27,6 +25,26 @@ public class CustomerController {
     @GetMapping({"/{customerId}"})
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId")UUID customerID){
         return new ResponseEntity<>(customerService.getCustomerByID(customerID), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto){
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","/api/v1/customer/"+customerDto.getId().toString());
+        return new ResponseEntity(headers,HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{customerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("customerId")UUID customerID, @RequestBody CustomerDto customerDto){
+        customerService.updateCustomer(customerID, customerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{customerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("customerId")UUID customerID){
+        customerService.deleteById(customerID);
     }
 
 
